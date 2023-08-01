3a118a2c3754c56a39fa074732dc4a1709285ab2
s <140096350+sssnoopyy@users.noreply.github.com>
2023-08-01 18:25:49 -0400

    -

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("continuebutton").addEventListener("click", function() {
+    document.getElementById("continuebutton").style.display = "none";
+    document.getElementById("restofsite").style.display = "block";
+    const audioplayer = document.getElementById("musicplayer");
+    audioplayer.play();
+    audioplayer.loop = true;
+});
