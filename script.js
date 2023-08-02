f268a52c934edc7de476b9e2cdeece8e28324b5a
s <140096350+sssnoopyy@users.noreply.github.com>
2023-08-02 01:50:01 -0400

    -

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 document.getElementById("continuebutton").addEventListener("click", function() {
     document.getElementById("continuebutton").style.display = "none";
     document.getElementById("restofsite").style.display = "block";
-        const site = document.getElementById("site");
-    site.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1133954199869194381/1136172140492771468/IMG_0426.jpg')";
     const files = [
         "./songs/music1.mp3",
         "./songs/music2.mp3",
