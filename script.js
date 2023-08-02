7a9d9b77e88bc1f55bfeb02e9de3e164df0a6431
s <140096350+sssnoopyy@users.noreply.github.com>
2023-08-01 22:54:23 -0400

    -

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 document.getElementById("continuebutton").addEventListener("click", function() {
     document.getElementById("continuebutton").style.display = "none";
     document.getElementById("restofsite").style.display = "block";
+    const files = [
+        "./songs/slut.mp3",
+        "./songs/ho.mp3",
+        "./songs/homie.mp3",
+        "./songs/bmpu.mp3",
+        "./songs/ddlg.mp3",
+    ];
+    const index = Math.floor(Math.random() * files.length);
     const audioplayer = document.getElementById("musicplayer");
+    audioplayer.src = files[index];
     audioplayer.play();
     audioplayer.loop = true;
 });
