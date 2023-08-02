13fcd7469d4665a323cd350ca12cb84c79838472
s <140096350+sssnoopyy@users.noreply.github.com>
2023-08-02 00:23:41 -0400

    -

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("continuebutton").addEventListener("click", function() {
     document.getElementById("continuebutton").style.display = "none";
     document.getElementById("restofsite").style.display = "block";
     const site = document.getElementById("site");
-    site.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1135592344847142952/1136148623374163968/IMG_0416.jpg')";
+    site.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1135592344847142952/1136152147705200750/IMG_0420.jpg')";
     const files = [
         "./songs/slut.mp3",
         "./songs/ho.mp3",
