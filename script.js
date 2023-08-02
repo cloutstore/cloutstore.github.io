554dfc12a7573c51d244fa262fc27f983fc31097
s <140096350+sssnoopyy@users.noreply.github.com>
2023-08-02 00:43:57 -0400

    -

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 document.getElementById("continuebutton").addEventListener("click", function() {
     document.getElementById("continuebutton").style.display = "none";
     document.getElementById("restofsite").style.display = "block";
-    const site = document.getElementById("site");
-    site.style.backgroundImage = "url('https://cdn.discordapp.com/attachments/1135592344847142952/1136152147705200750/IMG_0420.jpg')";
     const files = [
-        "./songs/slut.mp3",
-        "./songs/ho.mp3",
-        "./songs/homie.mp3",
-        "./songs/bmpu.mp3",
-        "./songs/ddlg.mp3",
+        "./songs/music1.mp3",
+        "./songs/music2.mp3",
     ];
     const index = Math.floor(Math.random() * files.length);
     const audioplayer = document.getElementById("musicplayer");
